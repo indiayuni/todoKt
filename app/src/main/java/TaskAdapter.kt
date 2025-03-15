bb5921931084091a709ed92badcab3f1f49b6b59
Indi Ayuni <ayunilestari148@gmail.com>
2025-03-15 20:58:55 +0800

    Task1- make a simple app

---
+++ app/src/main/java/TaskAdapter.kt
@@ -0,0 +1,36 @@
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.mobprog.tugass.R
+
+class TaskAdapter(private val taskList: MutableList<Task>) :
+    RecyclerView.Adapter<TaskAdapter.TaskViewHolder>() {
+
+    class TaskViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val taskName: TextView = itemView.findViewById(R.id.taskName)
+        val btnDelete: Button = itemView.findViewById(R.id.btnDelete)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_task, parent, false)
+        return TaskViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        val task = taskList[position]
+        holder.taskName.text = task.name
+
+        holder.btnDelete.setOnClickListener {
+            taskList.removeAt(position)
+            notifyDataSetChanged()
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return taskList.size
+    }
+}
